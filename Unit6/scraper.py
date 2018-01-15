1b1b2077f86695f04fda9c1aaedee5380306a119
jdwy215@gmail.com <jdwy215@gmail.com>
2018-01-15 18:29:40 -0500

    Unit6

---
+++ Unit6/scraper.py
@@ -0,0 +1,10 @@
+"""Task: from the archive (link below), follow each link, find the image
+in that linked page, and download the image
+url = https://apod.nasa.gov/apod/archivepix.html
+
+Concepts:
+    1. Download stuff => urllib
+    2. Parsing stuff out of html => BeautifulSoup
+"""
+
+import urllib
