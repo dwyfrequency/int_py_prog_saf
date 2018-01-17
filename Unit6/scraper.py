ed342ea10df25f9207d53ebbd0ed9e794b2463f6
jdwy215@gmail.com <jdwy215@gmail.com>
2018-01-16 21:32:39 -0500

    adding initial webscraper for downloading nasa images

--- Unit6/scraper.py
+++ Unit6/scraper.py
@@ -7,4 +7,38 @@ Concepts:
     2. Parsing stuff out of html => BeautifulSoup
 """
 
-import urllib
+import urllib.request
+from urllib.parse import urljoin # converts relative links to absolute
+from bs4 import BeautifulSoup
+
+# Dowload the index page 
+base_url = "https://apod.nasa.gov/apod/archivepix.html"
+download_directory = "/home/jdwy215/intPyProg_Jess_McK/Unit6/output/"
+resp = urllib.request.urlopen(base_url).read()
+
+# For each link on the index page: 
+for link in BeautifulSoup(resp, "lxml").findAll('a'):
+    print("Following link:", link)
+    href = urljoin(base_url, link["href"])
+
+    content = urllib.request.urlopen(href).read()
+    for img in BeautifulSoup(content, "lxml").findAll('img'):
+        img_href = urljoin(href, img["src"])
+        print("Downloading image:", img_href)
+
+        # split the image on the forward slash and take the last part
+        img_name = img_href.split("/")[-1]
+        urllib.request.urlretrieve(img_href, download_directory + img_name)
+
+# a tags start <a href> which gives us links
+# print(BeautifulSoup(resp, "lxml").findAll('a'))
+
+print(type(tags[0]))
+# print(tags[0])
+
+tag = tags[0]
+
+# calling the indiv tag with href with give us the underlying link
+print(tag["href"])
+
+
